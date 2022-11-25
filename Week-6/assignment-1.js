1816bb0541edbefe6f4f83f5802e4ae201e4493d
amitsankritya <amitsankritya@gmail.com>
2022-11-26 01:07:39 +0530

    max sum of contiguous array assignment done

---
+++ Week-6/assignment-1.js
@@ -0,0 +1,43 @@
+// Problem 6.1: Max Sum Contiguous Subarray
+
+let arr1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+
+const maxSum = function(arr) {
+
+  let temp = [];
+  let tempArr = [];
+  let sum = 0;
+  let tempSum = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+  
+    j = 0;
+  
+    while(j <= arr.length) {
+      if(tempArr.length == i) {
+        if(tempSum > sum) {
+          temp = tempArr;
+          sum = tempSum;
+        }
+  
+        if(j < arr.length) {
+          j = j - (i - 1);
+        }
+  
+        tempSum = 0;
+        tempArr = [];
+        
+      } else {
+        tempSum = tempSum + arr[j];
+        tempArr.push(arr[j]);
+        j++;
+      }
+    }
+      
+  }
+
+  return sum;
+}
+
+
+console.log(maxSum(arr1));
