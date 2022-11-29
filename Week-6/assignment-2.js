e0425b1488662ecbb45e31e10c24ddb763fa4017
amitsankritya <amitsankritya@gmail.com>
2022-11-29 23:27:12 +0530

    get matrix as sprial - in progress

---
+++ Week-6/assignment-2.js
@@ -0,0 +1,46 @@
+// Problem Description Given a matrix of m * n elements (m rows, n columns), 
+// return all elements of the matrix in spiral order.Example: 
+// Given the following matrix: [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ] You should return[1, 2, 3, 6, 9, 8, 7, 4, 5]
+
+/* [ 
+[ 1, 2, 3 ], 
+[ 4, 5, 6 ], 
+[ 7, 8, 9 ]
+] */
+
+// i = 0, j = 0
+// 00, 01, 02 => j = n-1 (first row)
+// 12, 22 => i = m - 1 (last column)
+// 21, 20 => j = 0 (last row)
+// 10, 11 => 
+
+let arr = [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ];
+let m = n = 3;
+let spiral = [];
+
+function getSpiral(arr, i, j, m, n,) {
+    
+    // print first row
+    for(let x = j; x < m; x++) {
+        spiral.push(arr[i][x]);
+    }
+
+    // print last column
+    for(let x = i+1; x < n; x++) {
+        spiral.push(arr[x][n-1]);
+    }
+
+    // print last row
+    for(let x = j+1; x >= 0; x--) {
+        spiral.push(arr[m-1][x]);
+    }
+
+    // print first column
+    for(let x = m - 1; x >= 0; x--) {
+        spiral.push(arr[x][j]);
+    }
+
+    return spiral;
+}
+
+console.log(getSpiral(arr, 0, 0, m, n));
